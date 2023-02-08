d8ae3c2aa9b6a300b49828448419a26c0c656c7c
sophia <sophiendalo@gmail.com>
2023-02-09 01:07:01 +0300

    completed assignment

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,56 @@
 def admin_login(username, password)
   # your code here
-end
+ 
+  if (username.upcase == "ADMIN" || username.downcase == "admin") && password == "12345"
+       "Access granted"
+   else 
+    "Access denied"
+    end
+  end
+  
+
+
 
 def hows_the_weather(temperature)
   # your code here
-end
+    if (temperature < 40)
+      'It\'s brisk out there!'
+    elsif (temperature >= 40 && temperature <= 65)
+      'It\'s a little chilly out there!'
+    elsif (temperature > 85)
+     'It\'s too dang hot out there!'
+    else
+     'It\'s perfect out there!'
+    end
+  end
+  
 
-def fizzbuzz(num)
+def fizzbuzz(number)
   # your code here
-end
+  result = ""
+  result << "Fizz" if number % 3 == 0
+  result << "Buzz" if number % 5 == 0
+  result.empty? ? number:result
+  end
+  
 
 def calculator(operation, num1, num2)
   # your code here
-end
+  case operation
+    when "+"
+      num1 + num2
+    when "-"
+      num1 - num2
+    when "*"
+      num1 * num2
+    when "/"
+      num1 / num2
+    else
+      puts "Invalid operation!"
+      nil
+    end
+  end
+  
+
+
 
